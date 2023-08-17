5a6b8f03e6ebaca5663e8ec8b9757ce186b62d07
Aleksei Scripnic <askripnik@gmail.com>
2023-08-17 10:42:58 +0200

    telegram.error.ChatMigrated: Group migrated to supergroup.

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ TELEGRAM_BASE_URL = "http://localhost:8081/bot"
 TELEGRAM_BASE_FILE_URL = "http://localhost:8081/file/bot"
 
 ALLOWED_TELEGRAM_CHAT_IDS = [
-    -951184410,  # testing group with hackathon participants
+    -1001685642492,  # testing group with hackathon participants
     -1001985729384,  # private group with @UgaChaka
 ]
 MEMBER_LIST_CACHE_TIME_SECONDS = 300  # 5 minutes
